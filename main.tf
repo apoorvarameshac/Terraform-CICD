f63395b2762ed9a500e6d1658ad6fa331a8bfc2e
apoorvarameshac <76146612+apoorvarameshac@users.noreply.github.com>
2025-11-11 17:55:12 +0530

    new code is added

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+resource "aws_ec2_instance" "CICD-server" {
+    ami = "ami-03695d52f0d883f65"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "terraform-server"
+    }
+}
+
